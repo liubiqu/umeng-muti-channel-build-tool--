f42ac4157e837a92bc22916bb8175fd9acb6c74c
liubiqu <liubiqu@hotmail.com>
2013-01-17 09:21:35 +0800

    增加安卓图标

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -571,6 +571,7 @@ namespace UmengChannel
             this.Controls.Add(this.projects);
             this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "MainForm";
             this.Text = "友盟Android渠道打包工具";
